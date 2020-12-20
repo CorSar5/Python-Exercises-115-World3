07cbde5bf51f1e39b7285fe8e6e033c3044a09a8
CorSar <1cesar.ccorreia@gmail.com>
2020-12-20 20:39:11 +0000

    ex-115

--- exercícios 72-115/ex115/lib/interface/__init__.py
+++ exercícios 72-115/ex115/lib/interface/__init__.py
@@ -1,3 +1,38 @@
+def leiaString(mens):
+    while True:
+        k = ['caralho','pila','penis','pica','piça','foda-se','fodase','pénis','fodasse','puta','cona','cabrao','vaca', 'aceite']
+        l = ('1','2','3','4','5','6','7','8','9','0')
+        try:
+            m = str(input(mens)).lower()
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar o seu nome. \033[m')
+            return 0
+        if m.isnumeric():
+            print('\033[31mERRO: por favor, digite um nome válido\033[m')
+            continue
+        #não atribuir números a nomes
+        '''for c in l:
+            if c in m:
+                print('\033[31mERRO: por favor, digite um nome válido\033[m')
+
+        if m in k:
+            print('\033[31mERRO: por favor, digite um nome válido\033[m')
+            continue'''
+        #impedir palavras calão escritas com mais 1 letra
+
+        if m in k:
+            print('\033[31mERRO: por favor, digite um nome válido\033[m')
+            continue
+        for n in k:
+            if n in m:
+                print('\033[31mERRO: por favor, digite um nome válido\033[m')
+                continue
+            if n == 'aceite':
+                break
+        else:
+            return m
+
+
 def leiaInt(msg):
     while True:
         try:
@@ -15,12 +50,11 @@ def leiaInt(msg):
 def linha(tam = 42):
     return '-' *tam
 
- 
+
 def cabecalho(txt):
     print(linha())
     print(txt.center(42))
     print(linha())
-
 
 
 def menu(lista):
--- exercícios 72-115/ex115/sistema.py
+++ exercícios 72-115/ex115/sistema.py
@@ -1,5 +1,4 @@
-from lib.arquivo import *
-from lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
 
 arq = 'cursoemvideo.txt'
@@ -8,18 +7,20 @@ if not arqExiste(arq):
 
 
 while True:
-    resposta = menu(['Visualizar Pessoas Registadas', 'Registar Nova Pessoa',  'Sair do Sistema'])
+    resposta = menu(['Visualizar Pessoas Registadas', 'Registar Nova Pessoa', 'Retirar Utilizador', 'Sair do Sistema'])
     # a resposta vai se tornar o return opc
     if resposta == 1:
         #opção de listar o conteúdo de um arquivo!
         lerArquivo(arq)
     elif resposta == 2:
         #opção de registrar uma pessoa
-        cabecalho(' NOVO CADASTRO ')
-        nome = str(input('Nome: '))
+        cabecalho('NOVO CADASTRO')
+        nome = leiaString('Nome: ')
         idade = leiaInt('Idade: ')
         cadastrar(arq, nome, idade)
     elif resposta == 3:
+        print('')
+    elif resposta == 4:
         cabecalho('Saindo do Sistema... Até mais!')
         break
     else:
