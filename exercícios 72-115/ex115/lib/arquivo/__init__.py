c7444cf4f2494ad6c8c2d7bc4dc42d5cead7c03e
CorSar <69777520+CorSar5@users.noreply.github.com>
2020-12-23 01:40:03 +0000

    Update __init__.py

--- exercícios 72-115/ex115/lib/arquivo/__init__.py
+++ exercícios 72-115/ex115/lib/arquivo/__init__.py
@@ -13,10 +13,44 @@ def arqExiste(nome):
 ##
 def criarArquivo(nome):
     try:
-        number = nome.length
-        a = open(number, nome, 'wt+')  #cria write text +
+        a = open(nome, 'wt+')  #cria write text +
         a.close()
     except:
         print('\033[31m.Houve um ERRO na criação do arquivo!\033[m')
     else:
         print(f'Arquivo {nome} criado com sucesso')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo')
+    else:
+        cabecalho('\033[36mPESSOAS REGISTRADAS\033[m')
+        for linha in a:
+            dado = linha.split(';')
+            dado[2] = dado[2].replace('\n','')
+            dado[0] = dado[0].replace('ï¿½','é')
+            print(f'{dado[0]:<10} {dado[1]:<15}{dado[2]:>4} anos')
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome='desconhecido', nome2 = 'desconhecido', idade=0):
+    try:
+        a = open(arq, 'at') #adicionar a de append
+    except:
+        print('Houve um ERRO na abertura do arquivo!')
+    else:
+        try:
+            a.write(f'{nome};{nome2};{idade}\n')
+        except:
+            print('Houve um ERRO na hora de escrever os dados!')
+        else:
+            print(f'Novo registro de {nome} {nome2} adicionado')
+            a.close()
+
+
+'''def eliminar(arq, nome='desconhecido, idade = 0'):
+    try'''
